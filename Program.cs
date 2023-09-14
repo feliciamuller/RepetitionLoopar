9f73a97b7e19601a1de9b0bd1dc44546a29f8540
Felicia Müller <felicia-muller@hotmail.com>
2023-09-14 11:38:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace RepetitionLoopar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hej! Välkommen till spelet.");
+
+            int myNumber = 5;
+            int attempts = 3;
+
+            while (attempts > 0)
+
+            {
+                Console.WriteLine("Gissa ett nummer: ");
+                Console.WriteLine($"Du har {attempts} försök kvar.");
+                attempts--;
+
+                int userInput = int.Parse(Console.ReadLine());
+
+                if (userInput == myNumber)
+                {
+                    Console.WriteLine("Du gissade rätt!");
+                    break;
+                }
+
+                else if (userInput > myNumber)
+                {
+                    if (userInput == myNumber +1)
+                    {
+                        Console.WriteLine("Nu bränns det, du gissade lite för högt bara.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Du gissade för högt");
+                    }
+                    
+                }
+
+                else if (userInput < myNumber)
+                {
+                    Console.WriteLine("Du gissade för lågt");
+                }
+
+
+            }
+            Console.WriteLine("Spelet är slut.");
+
+
+        }
+    }
+}
